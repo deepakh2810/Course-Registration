3d5b2d64ac4a1073293e7831ec2ef096501a7c27
Aditya Kushwah <akushwah@iu.edu>
2018-09-28 19:08:16 -0400

    semantic ui added

--- course-registration-system/src/App.js
+++ course-registration-system/src/App.js
@@ -3,7 +3,8 @@ import {Route} from 'react-router-dom'
 import HomePage from "./components/pages/HomePage"
 import LoginPage from "./components/pages/LoginPage"
 
-const App = () => <div>
+const App = () => 
+<div className="ui container">
     <Route path="/" exact component={HomePage} />
     <Route path="/login" exact component={LoginPage} />
 </div>
