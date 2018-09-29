f257925792ff080c3647509ad08d0e4e845a03a4
Aditya Kushwah <akushwah@iu.edu>
2018-09-29 12:11:20 -0400

    Redux Setup

--- course-registration-system/src/index.js
+++ course-registration-system/src/index.js
@@ -4,6 +4,19 @@ import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 import "semantic-ui-css/semantic.min.css";
 import registerServiceWorker from './registerServiceWorker';
+import {createStore, applyMiddleware} from 'redux';
+import{Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from "./rootReducer";
+import {composeWithDevTools} from 'redux-devtools-extension';
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+const store = createStore(
+    rootReducer, 
+    composeWithDevTools(applyMiddleware(thunk))
+);
+
+ReactDOM.render(<BrowserRouter>
+    <Provider store = {store}><App /></Provider>
+    </BrowserRouter>,
+ document.getElementById('root'));
 registerServiceWorker();
