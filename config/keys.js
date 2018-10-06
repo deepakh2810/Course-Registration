5f0c44634290bbfd7fbb80d7c5475ffc190826bf
askushwah <akushwah@iu.edu>
2018-10-06 08:12:59 -0400

    New login with express server integrated

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb://ransinha:ROCKNrolla1989@cluster0-shard-00-00-yurlj.mongodb.net:27017,cluster0-shard-00-01-yurlj.mongodb.net:27017,cluster0-shard-00-02-yurlj.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"
+};
