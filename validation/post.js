529bea20babf3f28ccfb895879047c003b351102
askushwah <akushwah@iu.edu>
2018-10-09 05:56:40 -0400

    Added post comment, like, unlike, delete API functionality

---
+++ validation/post.js
@@ -0,0 +1,20 @@
+const isEmpty = require("./is_empty");
+const Validator = require("validator");
+
+module.exports = function validatePostInput(data) {
+  let errors = {};
+
+  data.text = !isEmpty(data.text) ? data.text : "";
+
+  if (!Validator.isLength(data.text, { min: 10, max: 300 })) {
+    errors.text = "Post must be between 10 and 3000 character";
+  }
+  if (Validator.isEmpty(data.text)) {
+    errors.text = "Text field is required";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
