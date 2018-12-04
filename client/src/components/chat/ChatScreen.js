8fe5fd583d0c957399f16d2d155f866398dce1ca
Aditya Kushwah <speak2aditya@gmail.com>
2018-12-04 12:37:59 -0500

    Room id changed

--- client/src/components/chat/ChatScreen.js
+++ client/src/components/chat/ChatScreen.js
@@ -42,7 +42,7 @@ export default class ChatScreen extends Component {
       .then(currentUser => {
         this.setState({ currentUser });
         return currentUser.subscribeToRoom({
-          roomId: "19376935",
+          roomId: "19377240",
           messageLimit: 100,
           hooks: {
             onMessage: message => {
